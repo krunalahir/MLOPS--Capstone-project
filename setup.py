467b87dbd902cd0350f70bf4de1583c305bc7632
krunal05 <krunalahir0538@gmail.com>
2026-01-16 18:43:09 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this repo contains end to end mlops implementation',
+    author='Krunal Bhammar',
+    license='',
+)
